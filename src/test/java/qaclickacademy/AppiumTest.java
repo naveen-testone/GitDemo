17f8451565312c5b80218dc509bb0a2630f9a395
naveen <tt4526761@gmail.com>
2021-10-07 13:38:41 +0530

    first commit

---
+++ src/test/java/qaclickacademy/AppiumTest.java
@@ -0,0 +1,16 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	
+	@Test
+	public void NavtiveAppAndroid() {
+		System.out.println("NativeAppTest");
+	}
+	@Test
+	public void IosApps() {
+		System.out.println("IosApps");
+	}
+
+}
---
+++ src/test/java/qaclickacademy/SeleniumTest.java
@@ -0,0 +1,16 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+	
+	@Test
+	public void browserAutomation() {
+		System.out.println("browserAutomation");
+	}
+	@Test
+	public void elementUI() {
+		System.out.println("elementUI");
+	}
+
+}
